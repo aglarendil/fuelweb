dc02403e66bdfdb44d27f0ce4a9543b6de972de1
Evgeniy L <test0x0000@gmail.com>
2013-06-20 15:40:12 +0400

    Add before deployment controllers count checking

--- nailgun/nailgun/errors/__init__.py
+++ nailgun/nailgun/errors/__init__.py
@@ -11,6 +11,7 @@ default_messages = {
     "FailedProvisioning": "Failed to start provisioning",
     "WrongNodeStatus": "Wrong node status",
     "NodeOffline": "Node is offline",
+    "NotEnoughControllers": "Not enough controllers",
     # disk errors
     "NotEnoughFreeSpace": "Not enough free space",
     # network errors
